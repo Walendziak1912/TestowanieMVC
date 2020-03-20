75390abb51bef8cbbb737aeb31bfaaa8f9d72328
dlewandowski <dlewandowski@INSEQRDEV16>
2020-03-20 16:16:18 +0100

    Add project files.

---
+++ TestowanieMVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestowanieMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Imie = "Lis";
+            return View();
+        }
+        public ViewResult Odpowiedz()
+        {
+            return View();
+        }
+    }
+}
---
+++ TestowanieMVC/Models/Osoba.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestowanieMVC.Models
+{
+    public class Osoba
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Telefon { get; set; }
+        public bool? Potwierdzony { get; set; }
+        public int Wiek { get; set; }
+    }
+}
